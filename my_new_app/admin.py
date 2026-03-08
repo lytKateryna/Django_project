44882b89a308aa3283b1a73e89f38b25a64ecbda
lytKateryna <lyt.kateryna@gmail.com>
2026-03-08 22:46:20 +0100

    registred Models

--- my_new_app/admin.py
+++ my_new_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from my_new_app.models import Task, Category, SubTask
+
 
 # Register your models here.
+
+
+admin.site.register(Category)
+admin.site.register(Task)
+admin.site.register(SubTask)
