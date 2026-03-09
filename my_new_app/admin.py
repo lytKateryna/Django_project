7b9e708abc7adb241bedd25ce8b66f6fba53d80b
lytKateryna <lyt.kateryna@gmail.com>
2026-03-09 22:07:35 +0100

    added Models in admin

--- my_new_app/admin.py
+++ my_new_app/admin.py
@@ -4,7 +4,28 @@ from my_new_app.models import Task, Category, SubTask
 
 # Register your models here.
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+    list_filter = ['name']
+    list_per_page = 2
 
-admin.site.register(Category)
-admin.site.register(Task)
-admin.site.register(SubTask)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'categories','status', 'deadline']
+    search_fields = ['title']
+    list_filter = ['status', 'categories']
+    list_editable = ["status"]
+    list_per_page = 2
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'task','status', 'deadline']
+    search_fields = ['title']
+    list_filter = ['task', 'status']
+    list_editable = ["status"]
+    list_per_page = 2
+
+
